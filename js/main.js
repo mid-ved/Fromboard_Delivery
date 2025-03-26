644bdb3bc5f2c3960bb08fdc035853a04dd3eb36
Alexandr <f.alexandr08@mail.ru>
2025-03-26 14:27:03 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const slider = document.querySelector('.revs');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const slides = Array.from(slider.querySelectorAll('.rev'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    updateSlider();
+}
+
+function showNextSlide() {
+    slideIndex = (slideIndex + 1) % slideCount;
+    updateSlider();
+}
+
+function updateSlider() {
+    slides.forEach((slide, index) => {
+        if (index === slideIndex) {
+            slide.style.display = 'flex';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+updateSlider();
